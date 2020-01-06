f7bda691ee419d657c123cb8920aae58ab193ea7
toppmann2018 <59574369+toppmann2018@users.noreply.github.com>
2020-01-06 18:16:10 +0100

    Add files via upload

---
+++ ArUco-OpenGL.cpp
@@ -0,0 +1,243 @@
+//
+//  ArUco-OpenGL.cpp
+//
+//  Created by Jean-Marie Normand on 28/02/13.
+//  Copyright (c) 2013 Centrale Nantes. All rights reserved.
+//
+
+#include <iostream>
+#include "ArUco-OpenGL.h"
+#include <opencv2/imgproc/imgproc.hpp>
+
+void affichePerso() {
+	//Corps
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	for (int i = 4; i>= 1; i--) {
+		glVertex3f(personnage[i].x, personnage[i].z, personnage[i].y);
+	}
+	glEnd();
+   for (int i=0; i<4; i++){
+	   glBegin(GL_TRIANGLES);
+	for (int j=0; j<3;j++){
+		glVertex3f(personnage[facePersonnage[i][j]].x, personnage[facePersonnage[i][j]].z, personnage[facePersonnage[i][j]].y);
+	}
+		glEnd();
+   }
+
+   //Tête
+   glColor3f(0.0f, 0.0f, 0.0f);
+   glPushMatrix();
+   glTranslatef(0.0f ,0.0f, 0.115f);
+   glutSolidSphere(0.015f,10,10);
+   glPopMatrix();
+};
+// Constructor
+ArUco::ArUco(string intrinFileName, float markerSize) {
+   // Initializing attributes
+   m_IntrinsicFile= intrinFileName;
+   m_MarkerSize   = markerSize;
+   // read camera parameters if passed
+   m_CameraParams.readFromXMLFile(intrinFileName);
+   auto start = chrono::system_clock::now();
+}
+
+// Destructor
+ArUco::~ArUco() {}
+
+// Detect marker and draw things
+void ArUco::doWork(Mat inputImg) {
+   m_InputImage   = inputImg;
+   m_GlWindowSize = m_InputImage.size();
+   resize(m_GlWindowSize.width, m_GlWindowSize.height);
+}
+
+// Draw axis function
+void ArUco::drawAxis(float axisSize) {
+   // X
+   glColor3f (1,0,0);
+   glBegin(GL_LINES);
+   glVertex3f(0.0f, 0.0f, 0.0f); // origin of the line
+   glVertex3f(axisSize,0.0f, 0.0f); // ending point of the line
+   glEnd( );
+   
+   // Y
+   glColor3f (0,1,0);
+   glBegin(GL_LINES);
+   glVertex3f(0.0f, 0.0f, 0.0f); // origin of the line
+   glVertex3f(0.0f, axisSize, 0.0f); // ending point of the line
+   glEnd( );
+   
+   // Z
+   glColor3f (0,0,1);
+   glBegin(GL_LINES);
+   glVertex3f(0.0f, 0.0f, 0.0f); // origin of the line
+   glVertex3f(0.0f, 0.0f, axisSize); // ending point of the line
+   glEnd( );
+}
+
+// GLUT functionnalities
+
+// Drawing function
+void ArUco::drawScene(chrono::time_point<chrono::system_clock> start) {
+   // If we do not have an image we don't do anyhting
+   if (m_ResizedImage.rows==0)
+      return;
+   
+   // Setting up  OpenGL matrices
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(0, m_GlWindowSize.width, 0, m_GlWindowSize.height, -1.0, 1.0);
+   glViewport(0, 0, m_GlWindowSize.width , m_GlWindowSize.height);
+   glDisable(GL_TEXTURE_2D);
+   glPixelZoom( 1, -1);
+   
+   //////glRasterPos3f( 0, m_GlWindowSize.height  - 0.5f, -1.0f );
+   glRasterPos3f(0, m_GlWindowSize.height, -1.0f);
+   
+   glDrawPixels (m_GlWindowSize.width, m_GlWindowSize.height, GL_RGB, GL_UNSIGNED_BYTE, m_ResizedImage.ptr(0));
+   
+   // Enabling depth test
+   glEnable(GL_DEPTH_TEST);
+   
+   // Set the appropriate projection matrix so that rendering is done 
+   // in an environment like the real camera (without distorsion)
+   glMatrixMode(GL_PROJECTION);
+   double proj_matrix[16];
+   //m_CameraParams.glGetProjectionMatrix(m_InputImage.size(),m_GlWindowSize,proj_matrix,0.05,10);
+   m_CameraParams.glGetProjectionMatrix(m_ResizedImage.size(),m_GlWindowSize,proj_matrix,0.05,10);
+   glLoadIdentity();
+   glLoadMatrixd(proj_matrix);
+   
+   // Debug : outputting projection matrix
+   /*
+    std::cout<<"Projection Matrix"<<std::endl;
+   for(int i=0;i<4;i++) {
+      std::cout<<"| ";
+      for(int j=0;j<4;j++) {
+         std::cout<<proj_matrix[j*4+i]<<"\t\t\t";
+      }
+      std::cout<<" |"<<std::endl;
+   }
+    */
+   
+   //now, for each marker,
+   double modelview_matrix[16];
+   std::cout << "Number of markers: " << m_Markers.size() << std::endl;
+   
+   // For each detected marker
+   for (unsigned int m=0;m<m_Markers.size();m++)
+   {
+      m_Markers[m].glGetModelViewMatrix(modelview_matrix);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+      glLoadMatrixd(modelview_matrix);
+      
+      // Disabling light if it is on
+      GLboolean lightOn = false;
+      glGetBooleanv(GL_LIGHTING, &lightOn);
+      if(lightOn) {
+         glDisable(GL_LIGHTING);
+      }
+      
+      // Drawing axis
+      drawAxis(m_MarkerSize);
+
+	  float t=0;
+	  if (m_Markers.size() >= 2){
+		auto finish = chrono::system_clock::now();
+		chrono::duration<double> elapsed_seconde = (finish - start);
+		t = elapsed_seconde.count();
+		while (t >= m_Markers.size()*0.02){t-=m_Markers.size()*0.02;}	
+		cout << t;
+	  }
+	  else{start = chrono::system_clock::now();}
+	glTranslatef(0.0f,0.0f,t);
+	glRotatef(t,0.0f,0.0f,1.0f);
+	affichePerso();
+		// Re-enabling light if it is on
+	if(lightOn) {
+         glEnable(GL_LIGHTING);
+      }
+      
+      glPopMatrix();
+   }
+   
+   // Disabling depth test
+   glDisable(GL_DEPTH_TEST);
+   
+   m_pixels.create(m_GlWindowSize.height , m_GlWindowSize.width, CV_8UC3);
+   //use fast 4-byte alignment (default anyway) if possible
+   glPixelStorei(GL_PACK_ALIGNMENT, (m_pixels.step & 3) ? 1 : 4);
+   //set length of one complete row in destination data (doesn't need to equal img.cols)
+   glPixelStorei(GL_PACK_ROW_LENGTH, m_pixels.step/m_pixels.elemSize());
+   // Reading back the pixels
+   glReadPixels(0, 0, m_GlWindowSize.width , m_GlWindowSize.height, GL_BGR_EXT, GL_UNSIGNED_BYTE, m_pixels.data);
+   // Flip the pixels since OpenCV stores top to bottom and OpenGL from bottom to top
+   cv::flip(m_pixels, m_pixels, 0);
+}
+
+// Idle function
+void ArUco::idle(Mat newImage) {
+   // Getting new image
+   m_InputImage = newImage.clone();
+   
+   // Do that here ?
+   resize(m_InputImage.size().width, m_InputImage.size().height);
+   
+   // Undistort image based on distorsion parameters
+   m_UndInputImage.create(m_InputImage.size(),CV_8UC3);
+   
+   //transform color that by default is BGR to RGB because windows systems do not allow reading BGR images with opengl properly
+   cv::cvtColor(m_InputImage,m_InputImage,CV_BGR2RGB);
+   
+   //remove distorion in image
+   // Jim commented next line and added the clone line
+   //cv::undistort(m_InputImage,m_UndInputImage, m_CameraParams.CameraMatrix, m_CameraParams.Distorsion);
+   m_UndInputImage = m_InputImage.clone();
+   
+   //detect markers
+   m_PPDetector.detect(m_UndInputImage, m_Markers, m_CameraParams.CameraMatrix, Mat(), m_MarkerSize);
+   //m_PPDetector.detect(m_UndInputImage, m_Markers, m_CameraParams, m_MarkerSize);
+   
+   //resize the image to the size of the GL window
+   cv::resize(m_UndInputImage,m_ResizedImage,m_GlWindowSize);
+}
+
+// Resize function
+void ArUco::resize(GLsizei iWidth, GLsizei iHeight) {
+   m_GlWindowSize=Size(iWidth,iHeight);
+   
+   //not all sizes are allowed. OpenCv images have padding at the end of each line in these that are not aligned to 4 bytes
+   if (iWidth*3%4!=0) {
+      iWidth+=iWidth*3%4;//resize to avoid padding
+      resize(iWidth, m_GlWindowSize.height);
+   }
+   else {
+      //resize the image to the size of the GL window
+      //if (m_UndInputImage.rows!=0)
+         //cv::resize(m_UndInputImage, m_ResizedImage, m_GlWindowSize);
+   }
+}
+
+
+// Jim
+cv::Mat ArUco::getPixels() {
+   return m_pixels.clone();
+}
+
+// Test using ArUco to display a 3D cube in OpenCV
+void ArUco::draw3DCube(cv::Mat img, int markerInd) {
+   if(m_Markers.size() > markerInd) {
+      aruco::CvDrawingUtils::draw3dCube(img, m_Markers[markerInd], m_CameraParams); 
+   }
+}
+
+void ArUco::draw3DAxis(cv::Mat img, int markerInd) {
+   if(m_Markers.size() > markerInd) {
+      aruco::CvDrawingUtils::draw3dAxis(img, m_Markers[markerInd], m_CameraParams); 
+   }
+   
+}
---
+++ ArUco-OpenGL.h
@@ -0,0 +1,119 @@
+//
+//  ArUco-OpenGL.h
+//
+//  Created by Jean-Marie Normand on 28/02/13.
+//  Copyright (c) 2013 Centrale Nantes. All rights reserved.
+//
+
+#ifndef UserPerspectiveAR_ArUco_OpenGL_h
+#define UserPerspectiveAR_ArUco_OpenGL_h
+
+#include <iostream>
+
+#include <fstream>
+#include <sstream>
+#ifdef __APPLE__
+    #include <OPENGL/gl.h>
+    #include <GLUT/glut.h>
+#else
+	#include <Windows.h>
+    #include <GL/gl.h>
+    #include <GL/glut.h>
+#endif
+#include <aruco/aruco.h>
+#include <chrono>
+
+using namespace cv;
+using namespace aruco;
+
+class ArUco {
+// Attributes
+protected:
+   // Intrinsics file for the camera
+   string            m_IntrinsicFile;
+   // Size of the marker (in meters)
+   float             m_MarkerSize;
+   // The Marker Detector
+   MarkerDetector    m_PPDetector;
+   
+   // Vector of detected markers in the image
+   vector<Marker>    m_Markers;
+   
+   // OpenCV matrices storing the images
+   // Input Image
+   Mat               m_InputImage;
+   // Undistorted image
+   Mat               m_UndInputImage;
+   // Resized image
+   Mat               m_ResizedImage;
+   // Camera parameters
+   CameraParameters  m_CameraParams;
+   // Size of the OpenGL window size
+   Size              m_GlWindowSize;
+   
+   // Test to read OpenGL framebuffer
+   Mat               m_pixels;
+
+
+   
+// Methods
+public:
+   // Constructor
+   ArUco(string intrinFileName, float markerSize);
+   // Destructor
+   ~ArUco();
+   
+   // Detect marker and draw things
+   void  doWork(Mat inputImg);
+   
+   // Draw axis function
+   void  drawAxis(float axisSize);
+   
+   // GLUT functionnalities
+   
+   // Drawing function
+   void  drawScene(chrono::time_point<chrono::system_clock> start);
+   
+   // Idle function
+   void  idle(Mat newImage);
+   
+   // Resize function
+   void  resize(GLsizei iWidth, GLsizei iHeight);
+   
+   // Jim
+   Mat   getPixels();
+   
+   void  setDesiredImageSize(Size desiredSize);
+   void  setDesiredImageSize(int desiredWidth, int desiredHeight);
+   
+   // Test using ArUco to display a 3D cube in OpenCV
+   void  draw3DCube(cv::Mat img, int markerInd=0);
+   void  draw3DAxis(cv::Mat img, int markerInd=0);
+
+
+};
+
+typedef struct {
+   float x;
+   float y;
+   float z;
+} vertex;
+
+// Création des sommets d'un perso
+static vertex personnage[5]={
+   {0.0f,0.0f,0.0f},
+   {0.025,0.1f,0.025},
+   {0.025,0.1f,-0.025},
+   {-0.025,0.1f,-0.025},
+   {-0.025,0.1f,0.025}
+};
+
+// Création des faces d'un perso
+static int facePersonnage[4][3] ={
+   {0,1,2},
+   {0,2,3},
+   {0,3,4},
+   {0,4,1},
+};
+
+#endif
